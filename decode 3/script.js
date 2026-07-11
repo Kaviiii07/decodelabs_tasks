92c8b8e6cccc1e8aa169083ad1611cb97a59ee8b
Kaviiii07 <sskavitha2007@gmail.com>
2026-07-11 15:18:22 +0530

    Add files via upload

---
+++ decode 3/script.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function showMessage() {
+    alert("Welcome to my portfolio!");
+}
+
+function showSkills() {
+    alert("My Skills:\n\nHTML\nCSS\nJavaScript\nJava\nPython");
+}
